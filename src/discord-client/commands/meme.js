1e63e0d586f6650636a47557fa3b4fdc52425e7f
= <ruipaulo.unify@outlook.fr>
2023-09-28 06:20:30 +0200

    Better meme command, implemented paginator

--- src/discord-client/commands/meme.js
+++ src/discord-client/commands/meme.js
@@ -1,8 +1,9 @@
 // imports
-const axios = require("axios");
 const { Message } = require("discord.js");
-const embedUtility = require("../../modules/embedUtility");
 const messages = require("../../modules/messages");
+const embedUtility = require("../../modules/embedUtility");
+const { DiscordEmbedsPaginator } = require("../../modules/paginator");
+const { default: axios } = require("axios");
 
 module.exports = {
   name: "meme",
@@ -14,55 +15,72 @@ module.exports = {
    * @param {Array} args
    */
   async execute(msg, args) {
-    // quick ugly fix media not loading if is ivdeo (no preview on api)
-    let qMeme;
-    do {
-      const memeBank = [
-        "dankmemes",
-        "memes",
-        "funny",
-        "wholesomememes",
-        "comedymemes",
-      ];
-      const query = await axios.get(
-        `https://www.reddit.com/r/${
-          memeBank[Math.floor(Math.random() * memeBank.length)]
-        }.json?sort=top&t=week/`,
-      );
-      qMeme =
-        query.data.data.children[
-          Math.floor(Math.random() * query.data.data.children.length)
-        ].data;
-    } while (qMeme.is_video);
+    // Initialize variables
+    const memeBank = [
+      "dankmemes",
+      "memes",
+      "funny",
+      "wholesomememes",
+      "comedymemes",
+    ]; // subreddits
+    // max meme to fetch
+    const maxMeme = 5;
+    // embeds array
+    const embeds = [];
 
-    // new meme object
-    let meme = {
-      title: qMeme.title,
-      postLink: `https://www.reddit.com${qMeme.permalink}`,
-      url: qMeme.url,
-      ups: qMeme.ups,
-      comments: qMeme.num_comments,
-      author: qMeme.author,
-      subreddit: `Reddit /r${qMeme.subreddit}`,
-    };
-    const embeds = [
-      embedUtility.message(meme.title, meme.subreddit, {
-        name: meme.author,
-        url: `https://www.reddit.com/user/${meme.author}`,
-        iconURL: (
-          await axios.get(
-            `https://www.reddit.com/user/${meme.author}/about.json`,
-          )
-        ).data.data.icon_img.split("?")[0],
-      }),
-    ];
-    embeds[0].data.image = { url: meme.url };
-    embeds[0].data.url = meme.postLink;
-    embeds[0].data.footer.text = `👍 ${meme.ups} - 💭 ${meme.comments}\n${embeds[0].data.footer.text}`;
-    embeds[0].data.thumbnail = {
-      url: "https://www.redditinc.com/assets/images/site/reddit-logo.png",
-    };
-    msg.channel.send({ embeds: embeds }).catch((err) => {
+    // fetch memes
+    const query = await axios.get(
+      `https://www.reddit.com/r/${
+        memeBank[Math.floor(Math.random() * memeBank.length)]
+      }.json?sort=top&t=week/`,
+    );
+
+    // for each meme
+    let i = 0;
+    for (const meme in query.data.data.children) {
+      // get meme data
+      const memeData = query.data.data.children[meme].data;
+      // break if max meme reached
+      if (i++ >= maxMeme) break;
+      // skip if meme is video
+      if (memeData.is_video) continue;
+
+      // create embed
+      const embed = embedUtility.message(
+        memeData.title,
+        `Reddit r/${memeData.subreddit}`,
+        {
+          name: `u/${memeData.author}`,
+          url: `https://www.reddit.com/user/${memeData.author}`,
+          iconURL: (
+            await axios.get(
+              `https://www.reddit.com/user/${memeData.author}/about.json`,
+            )
+          ).data.data.icon_img.split("?")[0],
+        },
+      );
+      // edit embed
+      embed.data.image = { url: memeData.url };
+      embed.data.url = `https://www.reddit.com${memeData.permalink}`.replace(
+        "\\",
+        "/",
+      );
+      embed.data.footer.text = `👍 ${memeData.ups} - 💭 ${memeData.num_comments}\n${embed.data.footer.text}`;
+      embed.data.thumbnail = {
+        url: "https://www.redditinc.com/assets/images/site/reddit-logo.png",
+      };
+
+      // push embed to embeds array
+      embeds.push(embed);
+    }
+
+    // create paginator
+    const paginator = new DiscordEmbedsPaginator([], {
+      customEmbeds: embeds,
+    });
+
+    // send paginator message
+    await paginator.createPaginatorMessage(msg.channel).catch((err) => {
       console.error(err);
       return 1;
     });
